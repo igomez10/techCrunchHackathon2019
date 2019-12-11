49ca836a3f6fc4dc645bbbcf55670ffbd320e76d
Ignacio Gomez <ignacio.gomez@solarisbank.de>
2019-12-11 16:13:35 +0100

    working golang on browser

---
+++ wasm/go.mod
@@ -0,0 +1,19 @@
+module wasmExample
+
+go 1.13
+
+require (
+	github.com/gopherjs/gopherjs v0.0.0-20191106031601-ce3c9ade29de // indirect
+	github.com/kisielk/gotool v1.0.0 // indirect
+	github.com/neelance/astrewrite v0.0.0-20160511093645-99348263ae86 // indirect
+	github.com/neelance/sourcemap v0.0.0-20151028013722-8c68805598ab // indirect
+	github.com/shurcooL/go v0.0.0-20190704215121-7189cc372560 // indirect
+	github.com/shurcooL/go-goon v0.0.0-20170922171312-37c2f522c041 // indirect
+	github.com/shurcooL/httpfs v0.0.0-20190707220628-8d4bc4ba7749 // indirect
+	github.com/siongui/godom v0.0.0-20181006133514-8ba1cdb25192
+	github.com/spf13/cobra v0.0.5 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413 // indirect
+	golang.org/x/sys v0.0.0-20191210023423-ac6580df4449 // indirect
+	golang.org/x/tools v0.0.0-20191210221141-98df12377212 // indirect
+)
---
+++ wasm/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"syscall/js"
+	"time"
+)
+
+func main() {
+
+	go checkRunningLatestJob()
+
+	for {
+		//fmt.Println(time.Now().UnixNano())
+		time.Sleep(1 * time.Second)
+
+		res, err := http.Get("https://server.test-cors.org/server?id=2835400&enable=true&status=200&credentials=false")
+		if err != nil {
+			fmt.Println("failed to execute req ", err)
+		}
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println("failed to parse response")
+		}
+
+		document := js.Global().Get("document")
+
+		mock := document.Call("getElementById", "mock")
+
+		p := document.Call("createElement", "p")
+		p.Set("innerHTML", fmt.Sprintf("%+v", string(body)))
+
+		mock.Call("appendChild", p)
+		//fmt.Println(string(body))
+	}
+
+}
+
+func checkRunningLatestJob() {
+	var jobID string
+	//loc := js.Global().Get("location")
+	for {
+		time.Sleep(1 * time.Second)
+		res, err := http.Post("/api/ping", "", nil)
+		if err != nil {
+			fmt.Println("RELOADING COULDNT REACH HEALTH ENDPOINT")
+			//loc.Call("reload")
+		}
+
+		if jobID == "" {
+			jobID = res.Header.Get("jobID")
+		} else if res.Header.Get("jobID") != jobID {
+			//loc.Call("reload")
+		}
+	}
+}
